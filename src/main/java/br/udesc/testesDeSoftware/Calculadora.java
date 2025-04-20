8d0c34ed8226a8672ce8d01ec2cdbf1597ca82c7
duda <mullerermesm@gmail.com>
2025-04-19 23:45:23 -0300

    métodos calculadora simples

---
+++ src/main/java/br/udesc/testesDeSoftware/Calculadora.java
@@ -0,0 +1,17 @@
+package br.udesc.testesDeSoftware;
+
+public class Calculadora {
+    public int Somar (int a, int b){
+        return a+b;
+    }
+    public int Subtrair (int a, int b){
+        return a-b;
+    }
+    public int Multiplicar (int a, int b){
+        return a*b;
+    }
+    public int Dividir (int a, int b){
+        return a/b;
+    }
+
+}
